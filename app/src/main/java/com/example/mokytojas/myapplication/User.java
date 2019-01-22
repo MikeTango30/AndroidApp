95101d0e7f65a5bd757bf76a4ed4340ce8c0c3cb
algirdaskuslys <algirdas.kuslys@kitm.lt>
2019-01-22 11:32:55 +0200

    added shared Preferences for user login

--- app/src/main/java/com/example/mokytojas/myapplication/User.java
+++ app/src/main/java/com/example/mokytojas/myapplication/User.java
@@ -1,27 +1,79 @@
 package com.example.mokytojas.myapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class User {
     private String username;
     private String password;
+    private String email;
 
-    public User(String username, String password) {
-        this.username=username;
-        this.password=password;
-    }
+    private SharedPreferences sharedPreferences;
+    private static final String PREFERENCES_FILE_NAME ="com.example.mokytojas.myapplication";
+    private static final String USERNAME_KEY = "username";
+    private static final String PASSWORD_KEY = "password";
+    private static final String REMEMBER_ME_KEY = "rememberMe";
 
-    public void setUsername(String username) {
+    //Naujo vartotojo registracija
+    public User(String username, String password, String email) {
         this.username = username;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
+        this.email = email;
     }
 
-    public String getUsername() {
+    //Vartotojo prisijungimuj
+    public User(Context context) {
+        this.sharedPreferences = context.getSharedPreferences(User.PREFERENCES_FILE_NAME,
+                Context.MODE_PRIVATE);
+
+    }
+
+    public String getUsernameForRegistration() {
         return username;
     }
 
-    public String getPassword() {
+    public void setUsernameForRegistration(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordForRegistration() {
         return password;
     }
+
+    public void setPasswordForRegistration(String password) {
+        this.password = password;
+    }
+
+    public String getEmailForRegistration() {
+        return email;
+    }
+
+    public void setEmailForRegistration(String email) {
+        this.email = email;
+    }
+
+    public String getUsernameForLogin() {
+        return this.sharedPreferences.getString(USERNAME_KEY,"");
+    }
+
+    public void setUsernameForLogin(String username) {
+        this.sharedPreferences.edit().putString(USERNAME_KEY,username).commit();
+    }
+
+    public String getPasswordForLogin() {
+        return this.sharedPreferences.getString(PASSWORD_KEY,"");
+    }
+
+    public void setPasswordForLogin(String password) {
+        this.sharedPreferences.edit().putString(PASSWORD_KEY,password).commit();
+    }
+
+    public boolean isRemembered(){
+        return this.sharedPreferences.getBoolean(REMEMBER_ME_KEY,false);
+    }
+
+    public void setRememberMeKey(boolean rememberMeKey){
+        this.sharedPreferences.edit().putBoolean(REMEMBER_ME_KEY,rememberMeKey).commit();
+    }
+    
 }
