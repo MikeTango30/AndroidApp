0197337d3373f4fa487e91d160b4c070115eafb7
algirdaskuslys <algirdas.kuslys@kitm.lt>
2019-01-28 16:26:13 +0200

    added new entry (pokemon) form.

--- app/src/main/java/com/example/mokytojas/myapplication/SearchActivity.java
+++ app/src/main/java/com/example/mokytojas/myapplication/SearchActivity.java
@@ -1,7 +1,10 @@
 package com.example.mokytojas.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         setTitle(R.string.searchTitle);
+
+        Button insertBtn = findViewById(R.id.insertBtn);
+
+        insertBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent goToNewEntryActivity = new Intent(SearchActivity.this, NewEntryActivity.class);
+                startActivity(goToNewEntryActivity);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mokytojas/myapplication/Validation.java
+++ app/src/main/java/com/example/mokytojas/myapplication/Validation.java
@@ -6,6 +6,8 @@ import java.util.regex.Pattern;
 public class Validation {
     private static final String VALID_USERNAME_PATTERN = "^[A-Za-z]{5,30}$";
     private static final String VALID_PASSWORD_PATTERN = "^[A-Za-z0-9!@#$%^&*]{5,30}$";
+    private static final String VALID_ENTRYNAME_PATTERN = "^[A-Za-z]{2,15}$";
+    private static final String VALID_ENTRYWEIGHT_PATTERN = "^[0-9]{1,4}$";
 
     public static boolean isValidUsername(String username) {
         Pattern credentialPattern = Pattern.compile(VALID_USERNAME_PATTERN);
@@ -16,6 +18,17 @@ public class Validation {
     public static boolean isValidPassword(String password) {
         Pattern credentialPattern = Pattern.compile(VALID_PASSWORD_PATTERN);
         Matcher credentialMatcher = credentialPattern.matcher(password);
+        return credentialMatcher.find();
+    }
+
+    public static boolean isValidEntryName(String entryName) {
+        Pattern credentialPattern = Pattern.compile(VALID_ENTRYNAME_PATTERN);
+        Matcher credentialMatcher = credentialPattern.matcher(entryName);
+        return credentialMatcher.find();
+    }
+    public static boolean isValidEntryWeight(String entryWeight) {
+        Pattern credentialPattern = Pattern.compile(VALID_ENTRYWEIGHT_PATTERN);
+        Matcher credentialMatcher = credentialPattern.matcher(entryWeight);
         return credentialMatcher.find();
     }
 }
